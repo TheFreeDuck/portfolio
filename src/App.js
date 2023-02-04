d407207a530af5e799eb01b3f3c12f7d2764e016
TheFreeDuck <fredrikthorcewers@gmail.com>
2023-02-04 15:11:47 +0100

    added song

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+const MusicPage = () => {
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    fetch("../resources/music/scientist.mp3")
+      .then((response) => response.json())
+      .then((data) => {
+        setSongs(data);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>WELCOME TO MY APPAGE</code>
-        </p>
+    <div>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Music</a>
+            </li>
+            <li>
+              <a href="#">Programs</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+          </ul>
+        </nav>
       </header>
+      <main>
+        <h1>Music</h1>
+        <ul>
+          {songs.map((song) => (
+            <li key={song.id}>
+              <a href={`/songs/${song.filename}`}>{song.title}</a>
+            </li>
+          ))}
+        </ul>
+      </main>
     </div>
   );
-}
+};
 
-export default App;
+export default MusicPage;
